f41ea34755e27e2676eea08caea71fc8768a6056
vizzv <vishwa10032003@gmail.com>
2023-06-01 16:16:18 +0530

    ..

---
+++ constuctor/src/com/constuctor/constuctor.java
@@ -0,0 +1,13 @@
+package com.constuctor;
+
+public class constuctor {
+	public constuctor() {
+		System.out.println("in constuctor");
+	}
+	public static void main(String[] args) {
+		
+		constuctor cs= new constuctor();
+		
+	}
+
+}
